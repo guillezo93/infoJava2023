d1d2908cd0a4084fd020bd9fe9d85b2b78c15974
Guille Zacarias <guillezacariaso@gmail.com>
2023-09-24 19:06:41 -0300

    Guia de trabajos prácticos 01

--- TrabajosPracticos01/IniciadoJavaJedi/CalculoDeDivisibilidad.java
+++ TrabajosPracticos01/IniciadoJavaJedi/CalculoDeDivisibilidad.java
@@ -11,8 +11,10 @@ public class CalculoDeDivisibilidad {
 
         resultado = num1 / num2;
 
-        if (resultado == 0) {
-            System.out.println(resultado);
+        if (num1 % num2 == 0) {
+            System.out.println("El valor de " + num1 + ", si es divisible por " + num2);
+        } else {
+            System.out.println("No es divisible");
         }
     }
 }
---
+++ TrabajosPracticos01/IniciadoJavaJedi/TrianguloDeAsteriscos.java
@@ -0,0 +1,19 @@
+package TrabajosPracticos01.IniciadoJavaJedi;
+
+public class TrianguloDeAsteriscos {
+    public static void main(String[] args) {
+        triangulo(4);
+
+    }
+
+    public static void triangulo( int filas) {
+        int i,j;
+
+        for (i = 1; i<= filas; i++) {
+            for (j=1; j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
